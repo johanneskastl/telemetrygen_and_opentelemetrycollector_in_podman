51964e0715ecdbb0340bffe1f3aca433ad45a9e2
Johannes Kastl <johannes.kastl@ars.de>
2024-04-17 13:08:23 +0200

    run_opentelemetrycollector.sh: use 0.98.0 version of opentelemetry-collector-contrib

--- run_opentelemetrycollector.sh
+++ run_opentelemetrycollector.sh
@@ -4,4 +4,4 @@ podman run \
     --rm \
     -p 4317:4317 \
     -v "${PWD}"/config.yaml:/etc/otelcol-contrib/config.yaml \
-    ghcr.io/open-telemetry/opentelemetry-collector-releases/opentelemetry-collector-contrib:0.86.0
+    ghcr.io/open-telemetry/opentelemetry-collector-releases/opentelemetry-collector-contrib:0.98.0
